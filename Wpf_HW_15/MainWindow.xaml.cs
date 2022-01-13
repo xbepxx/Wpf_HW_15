03d8e80178c92cf33eba85536ac2b49eda77488f
xbepxx@gmail.com <xbepxx@gmail.com>
2022-01-13 12:03:37 +0300

    Добавьте файлы проекта.

---
+++ Wpf_HW_15/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_HW_15
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Obj.ClearValue(FlowDocumentScrollViewer.DocumentProperty);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Create))
+            {
+                XamlWriter.Save(Obj.Document, fs);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (FileStream fs = File.Open("1.xaml", FileMode.Open))
+            {
+                Obj.Document = XamlReader.Load(fs) as FlowDocument;
+            }
+        }
+    }
+}
